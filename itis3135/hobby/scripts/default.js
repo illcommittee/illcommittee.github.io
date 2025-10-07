ba2fe461e2713e98f816a2da85521fb925cb4ff7
Nathaniel Gustafson <gustafson.nate3@gmail.com>
2025-10-06 22:30:19 -0400

    Hobby page start

---
+++ itis3135/hobby/scripts/default.js
@@ -0,0 +1,5 @@
+function myFunction(){
+    document.getElementById("demo").innerHTML = "Hello JavaScript!"
+    console.log("I'm writing to the console")
+    alert("Hello from the default area of .js!");
+}
