63722cf9c714e019ab915fc21e4a8f640c2eb9fb
Nathaniel Gustafson <gustafson.nate3@gmail.com>
2025-10-18 01:00:20 -0400

    Made changes to hobby website

--- itis3135/hobby/scripts/default.js
+++
@@ -1,5 +0,0 @@
-function myFunction(){
-    document.getElementById("demo").innerHTML = "Hello JavaScript!"
-    console.log("I'm writing to the console")
-    alert("Hello from the default area of .js!");
-}
